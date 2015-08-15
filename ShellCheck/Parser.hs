f85441add9ef4f6b739dcc34d79ad9070f86967b
Vidar Holen <spam@vidarholen.net>
2015-08-15 13:25:34 -0700

    Removed unused pragma.

--- ShellCheck/Parser.hs
+++ ShellCheck/Parser.hs
@@ -17,7 +17,7 @@
     yOU should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
-{-# LANGUAGE NoMonomorphismRestriction, TemplateHaskell, FlexibleContexts #-}
+{-# LANGUAGE NoMonomorphismRestriction, TemplateHaskell #-}
 module ShellCheck.Parser (parseScript, runTests) where
 
 import ShellCheck.AST
