f054e2e2cc94bb4a5985f9e4225253faad591faa
Matteo Beniamino <beniamino@tautologica.org>
2015-08-23 10:55:18 +0200

    Added FlexibleContexts extension in Parser.hs to fix compilation.

--- ShellCheck/Parser.hs
+++ ShellCheck/Parser.hs
@@ -17,7 +17,7 @@
     yOU should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -}
-{-# LANGUAGE NoMonomorphismRestriction, TemplateHaskell #-}
+{-# LANGUAGE NoMonomorphismRestriction, TemplateHaskell, FlexibleContexts #-}
 module ShellCheck.Parser (parseScript, runTests) where
 
 import ShellCheck.AST
